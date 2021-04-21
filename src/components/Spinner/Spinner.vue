b70c238c0c5589f86e74c6922da95102855fff84
Eldar Babaev <el.glagius@gmail.com>
2021-04-21 14:13:16 +0700

    Added Spinner

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class='loader'></div>
+</template>
+<script lang="ts">
+export default {
+
+};
+</script>
+<style lang="scss">
+  .loader,
+  .loader:before,
+  .loader:after {
+    border-radius: 50%;
+  }
+  .loader {
+    color: linear-gradient(90deg, rgba(255,102,71,1) 0%, rgba(214,0,143,1) 50%);;
+    font-size: 11px;
+    text-indent: -99999em;
+    margin: 55px auto;
+    position: relative;
+    width: 10em;
+    height: 10em;
+    box-shadow: inset 0 0 0 1em;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+  }
+  .loader:before,
+  .loader:after {
+    position: absolute;
+    content: '';
+  }
+  .loader:before {
+    width: 5.2em;
+    height: 10.2em;
+    background: white;
+    border-radius: 10.2em 0 0 10.2em;
+    top: -0.1em;
+    left: -0.1em;
+    -webkit-transform-origin: 5.1em 5.1em;
+    transform-origin: 5.1em 5.1em;
+    -webkit-animation: load2 2s infinite ease 1.5s;
+    animation: load2 2s infinite ease 1.5s;
+  }
+  .loader:after {
+    width: 5.2em;
+    height: 10.2em;
+    background: white;
+    border-radius: 0 10.2em 10.2em 0;
+    top: -0.1em;
+    left: 4.9em;
+    -webkit-transform-origin: 0.1em 5.1em;
+    transform-origin: 0.1em 5.1em;
+    -webkit-animation: load2 2s infinite ease;
+    animation: load2 2s infinite ease;
+  }
+  @-webkit-keyframes load2 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes load2 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+</style>
