e4f16f99389ecaee7dc81437500773dc854f7509
Eldar Babaev <el.glagius@gmail.com>
2021-04-22 07:22:52 +0700

    Test fix

--- src/components/PromtCard/PromtCard.spec.ts
+++ src/components/PromtCard/PromtCard.spec.ts
@@ -34,7 +34,7 @@ describe('PromtCard component', () => {
       .should('have.attr', 'src')
       .and('not.equal', image);
   });
-  it.only('show default image, if the address isn\'t specified', () => {
+  it('show default image, if the address isn\'t specified', () => {
     const image = 'http://some-other-address.com/image.png';
 
     mount(PromtCard, { propsData: { name, contactInfo, image: null } });
